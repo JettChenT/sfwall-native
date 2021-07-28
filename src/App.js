f78636e7ca3c79692a608ae63353788043b2fdea
JettChenT <jettchen12345@gmail.com>
2021-07-28 17:25:52 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Cookies from 'universal-cookie';
+import React, { useEffect, useState } from 'react';
+const { ipcRenderer } = window.require('electron')
 
 function App() {
+  let [token, setToken] = useState(ipcRenderer.sendSync('get-token'));
+  const wid = window.screen.width * window.devicePixelRatio;
+  const hei = window.screen.height * window.devicePixelRatio;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        !token &&
+        <h1>Your Token could be found in your profile at <a href="https://scan4wall.xyz">our webapp</a></h1>
+      }
+      <input 
+        className="input" 
+        type="text" 
+        placeholder="Set SFW Token"
+        value={token}
+        onChange={e => setToken(e.target.value)}
+      />
+      <button onClick={() => ipcRenderer.send('set-token',token)}>Save Token</button>
+      {
+        token &&
+        <button onClick={() => ipcRenderer.send('recommend',wid,hei)}>RECOMMEND!</button>
+      }
     </div>
   );
 }
---
+++ utils/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    FETCH_TEXT_FROM_STORAGE: 'fetch-text-from-storage',
+    SAVE_TEXT_TO_STORAGE: 'save-text-to-storage',
+    HANDLE_FETCH_TEXT_FROM_STORAGE: 'handle-fetch-text-from-storage',
+    HANDLE_SAVE_TEXT_TO_STORAGE: 'handle-save-text-to-storage',
+}
